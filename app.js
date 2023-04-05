dd5c7264786c60ee9cdb4711409f6a44d3e22f11
Jonathan Davila <jonathandavila@Jonathans-MacBook-Air.local>
2023-04-04 19:31:41 -0700

    feat: added movement along x-axis

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const game = document.querySelector('#game');
 const ctx = game.getContext('2d');
 
-let player;
+let user;
+let startingPlatform;
 let cloud; // theme of platforms will be clouds (??)
 
 
@@ -23,7 +24,7 @@ class Player {
         this.alive = true;
 
         this.render = () => {
-            // ctx.fillStyle = ; // player sprite will go here using .drawImage() => no need for .fillStyle or .fillRect
+            ctx.fillStyle = 'black'; // player sprite will go here using .drawImage() => no need for .fillStyle or .fillRect
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
@@ -43,11 +44,38 @@ class Platform {
     }
 }
 
+// EVENT LISTENERS
+window.addEventListener('DOMContentLoaded', function () {
+    // starting position
+    user = new Player(200, 200, 20, 20);
+    startingPlatform = new Platform((user.x-10), (user.y+30), 40, 10); // starting platform should be placed under player to avoid instant loss
+    user.render();
+    startingPlatform.render();
 
-let player1 = new Player(10, 10, 20, 20);
-player1.render();
-console.log(player1);
+    // run the game loop
+    const runGame = setInterval(gameLoop, 30);
+});
 
-let platform1 = new Platform(10, 40, 40, 20);
-platform1.render();
-console.log(platform1);
+document.addEventListener('keydown', movementHandler);
+
+function gameLoop() {
+    ctx.clearRect(0, 0, game.width, game.height);
+    user.render();
+    startingPlatform.render();
+}
+
+
+function movementHandler(e) {
+    if (e.key === 'ArrowLeft' || e.key === 'a' || e.key === 'A') {
+        if ((user.x - 5) >= 0 ) {
+            user.x -= 5;
+        }
+    } else if (e.key === 'ArrowRight' || e.key === 'd' || e.key === 'D') {
+        if ((user.x + 5 <= (game.width - user.width))) {
+            user.x += 5;
+        }
+    }
+}
+
+
+// if the player moves up in y, the platforms move down in y (opposite of this since down is positive in this frame of reference)
